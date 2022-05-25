47ac2738a7ee5ce61429cb37d6ed0cf2615639e8
Foo Yong Qi <yongqifoo@gmail.com>
2022-05-25 03:11:03 +0000

    discussion

--- examples/test3.java
+++ examples/test3.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
-class A<T> extends D<T> {
+class Whatever {
   void main() {
-    D<Integer> d;
+
+    D<? extends Integer> d = new D<>();
     B<List<Integer>> b;
     b = d;
     A<String> a;
     B<List<String>> b2;
     b2 = a;
+    D<Object> dd;
+    A<Object> aa;
+    dd = aa;
+
   }
 }
+
